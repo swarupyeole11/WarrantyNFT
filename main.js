26b3312add5c3c11b8815bbdaa220953ca4d7320
adityagoel28 <adityagoel2002@gmail.com>
2022-07-20 22:11:39 +0530

    error fixed

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-Moralis.initialize("7LVcpr1IeYLThaA8PSnVgdO78hlBU33gH3qan8s3");
-Moralis.serverUrl = "https://msfamyuq5cz2.usemoralis.com:2053/server";
+// Moralis.initialize("7LVcpr1IeYLThaA8PSnVgdO78hlBU33gH3qan8s3");
+// Moralis.serverURL = "https://msfamyuq5cz2.usemoralis.com:2053/server";
 
-async function initializeApp(){
-    let currentUser = Moralis.User.current();
-    if(!currentUser){
-        currentUser = await Moralis.Web3.authenticate();
-    }
-    const options = {
-      address: "0x2ccd43e32a508e4ec5d2eec7c5e8b7212a656790",
-      chain: "rinkeby",
+// async function initializeApp(){
+//     let currentUser = Moralis.User.current();
+//     if(!currentUser){
+//         currentUser = await Moralis.Web3.authenticate();
+//     }
+//     const options = {
+//       address: "0x2ccd43e32a508e4ec5d2eec7c5e8b7212a656790",
+//       chain: "rinkeby",
+//     };
+//     let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
+//     console.log(NFTs); 
+// }
+
+// initializeApp();        
+
+
+const appId = ("7LVcpr1IeYLThaA8PSnVgdO78hlBU33gH3qan8s3");
+const serverUrl = "https://msfamyuq5cz2.usemoralis.com:2053/server";
+Moralis.start({ serverUrl , appId })
+
+async function initializeApp() {
+  let currentUser = Moralis.User.current();
+  if (!currentUser) {
+    currentUser = await Moralis.Web3.authenticate();
+  }
+    const options = { 
+      address: "0x2ccd43e32a508e4ec5d2eec7c5e8b7212a656790", 
+      chain: "rinkeby" 
     };
-    let NFTs = await Moralis.Web3API.token.getALLTokenIds(options);
-    console.log(NFTs); 
+    const NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
+    console.log(NFTs);
 }
 
-initializeApp();        
+initializeApp();
