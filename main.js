74f8d43c26eeeda1ca578461719a5fab5e8b2070
adityagoel28 <adityagoel2002@gmail.com>
2022-07-24 17:02:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,31 +23,65 @@ Moralis.start({ serverUrl , appId })
 
 
 function renderInventory(NFTs){
+  console.log('aditya');
   console.log(NFTs);
   const parent = document.getElementById("app"); 
-  console.log(NFTs.length); 
-  for(let i=0;i<NFTs.length;i++){
-    console.log(NFTs);  
-    const nft = NFTs[i];
-    let htmlstring = `
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${nft.metadata.image}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${nft.metadata.name}</h5>
-      <p class="card-text">${nft.metadata.description}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  `
+  console.log(NFTs.result.length); 
+  // {"image":"https://msfamyuq5cz2.usemoralis.com/tokyo.png","description":"the artwork of a solidity developer","name":"Sanskar's artwork"}
 
-    let col=document.createElement("div");
-    col.className = "col col-md-3"  
-    col.innerHTML = htmlstring;
-    parent.appendchild(col);
-    console.log("a");
-
+  for(let i=0;i<NFTs.result.length;i++){
+    const myObj = JSON.parse(NFTs.result[i].metadata);
+    const div = document.createElement("div");
+    div.className = "card";
+    const img = document.createElement("img");
+    img.src = myObj.image;
+    img.className = "card-img-top";
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.innerHTML = myObj.name;
+    const cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerHTML = myObj.description;
+    const button = document.createElement("button");
+    button.className = "btn btn-primary";
+    button.innerHTML = "Buy";
+    button.onclick = function(){
+      Moralis.Web3API.token.buyToken(NFTs.result[i].id);
+    }
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(button);
+    div.appendChild(img);
+    div.appendChild(cardBody);
+    parent.appendChild(div);
   }
 }
+  // console.log('data', myObj.image);
+//   for(let i=0;i<NFTs.result.length;i++){
+//     console.log(NFTs);  
+//     const nft = NFTs;
+//     const myObj = JSON.parse(nft.result[i].metadata);
+//     let htmlstring = `
+//     <div class="card" style="width: 18rem;">
+//     <img class="card-img-top" src="${myObj.image}" alt="Card image cap">
+//     <div class="card-body">
+//       <h5 class="card-title">${myObj.name}</h5>
+//       <p class="card-text">${myObj.description}</p>
+//       <a href="#" class="btn btn-primary">Go somewhere</a>
+//     </div>
+//   </div>
+//   `
+
+//     let col=document.createElement("div");
+//     col.className = "col col-md-3"  
+//     col.innerHTML = htmlstring;
+//     parent[0].appendchild(col);
+//     console.log("a");
+
+//   }
+// }
 
 async function initializeApp() {
   let currentUser = Moralis.User.current();
@@ -58,10 +92,9 @@ async function initializeApp() {
       address: "0x37156b89ca3b12c1949c0c60caed76b59b969b04",
       chain: "rinkeby",
     };
-      const  NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
+      let  NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
       console.log(NFTs);
     
-   
     renderInventory(NFTs);
 }
 
