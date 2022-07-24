23e1fae6adebd24af760bdda07d41323a3671c37
unknown <sanskarchandra26@gmail.com>
2022-07-24 16:00:36 +0530

    NFT

--- main.js
+++ main.js
@@ -23,8 +23,11 @@ Moralis.start({ serverUrl , appId })
 
 
 function renderInventory(NFTs){
-  const parent = document.getElementById("app");  
+  console.log(NFTs);
+  const parent = document.getElementById("app"); 
+  console.log(NFTs.length); 
   for(let i=0;i<NFTs.length;i++){
+    console.log(NFTs);  
     const nft = NFTs[i];
     let htmlstring = `
     <div class="card" style="width: 18rem;">
@@ -41,20 +44,24 @@ function renderInventory(NFTs){
     col.className = "col col-md-3"  
     col.innerHTML = htmlstring;
     parent.appendchild(col);
+    console.log("a");
 
   }
 }
+
 async function initializeApp() {
   let currentUser = Moralis.User.current();
   if (!currentUser) {
     currentUser = await Moralis.Web3.authenticate();
    }
-    const options = { 
-      address: "0x2ccd43e32a508e4ec5d2eec7c5e8b7212a656790", 
-      chain: "rinkeby" 
+    const options = {
+      address: "0x37156b89ca3b12c1949c0c60caed76b59b969b04",
+      chain: "rinkeby",
     };
       const  NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
       console.log(NFTs);
+    
+   
     renderInventory(NFTs);
 }
 
