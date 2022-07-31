e4921e53cdff93c2a0108159062fc093791a889c
adityagoel28 <adityagoel2002@gmail.com>
2022-07-31 20:37:13 +0530

    frontend updated

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -6,11 +6,11 @@ import market from './market.png'
 
 const Navigation = ({ web3Handler, account }) => {
     return (
-        <Navbar expand="lg" bg="secondary" variant="dark">
+        <Navbar expand="lg" bg="primary" variant="dark">
             <Container>
-                <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
+                <Navbar.Brand href="https://www.flipkart.com/">
                     <img src={market} width="40" height="40" className="" alt="" />
-                    &nbsp; DApp NFT Marketplace
+                    &nbsp; Flipkart Warranty System
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
